1bd688d71c514abc7fa01a914b76f4654161b7d9
itsoluwatobby <oluwasamuel050@gmail.com>
2024-03-14 14:46:49 +0100

    task completed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""
+Write a method that determines if all the boxes can be opened.
+"""
+
 
 def canUnlockAll(boxes):
-
+    """A function that determines if a box is opened or closed"""
     keys = set([0])
     visited = [0]
 
     while visited:
         current = visited.pop(0)
 
-        for key in boxes[current]:
-            if key < len(boxes) and key not in keys:
-                keys.add(key)
-                visited.append(key)
+        for i in boxes[current]:
+            if i < len(boxes) and i not in keys:
+                keys.add(i)
+                visited.append(i)
 
     return len(keys) == len(boxes)
